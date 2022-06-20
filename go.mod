e636776dbbd90add2604c080f043affb5e86dd0b
arfian-rp <arfianpradana07@gmail.com>
2022-06-20 18:44:33 +0700

    test go app

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/arfian-rp/app-sayhello
+
+go 1.18
+
+// require github.com/arfian-rp/go-sayhello v1.5.0
+
+require github.com/arfian-rp/go-sayhello/v2 v2.0.0 
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	// go_sayhello "github.com/arfian-rp/go-sayhello"
+	go_sayhello "github.com/arfian-rp/go-sayhello/v2"
+)
+
+func main() {
+	fmt.Println(go_sayhello.SayHello("Yanto", "Kurniawan"))
+}
